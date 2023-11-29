8f50488a96a014bd5c32d0a01c9bf6d579bbd858
luucaasrulo <lucas___1997@hotmail.com>
2023-11-29 20:22:34 -0300

    Lucas

--- a_proyecto/urls.py
+++ a_proyecto/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from .views import *
 
 
@@ -8,4 +13,7 @@ urlpatterns = [
     path("",index, name='inicio'),
     path('contacto/',contacto, name='contacto'),
     path('usuarios/', include('apps.usuarios.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)

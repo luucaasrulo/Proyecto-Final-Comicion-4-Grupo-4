67e9a6a1237cb4b02391dedf68a3abf37a8f93cf
luucaasrulo <lucas___1997@hotmail.com>
2023-11-30 20:39:57 -0300

    Lucas

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+from .models import Categoria
 # Create your views here.
+
+class AgregarCatergoria(CreateView):
+    model = Categoria
+    files = {'nombres'}
+    template_name = 'post/agregar_categoria.html'
+    success_url = reverse_lazy('inicio')
+
+
+
+

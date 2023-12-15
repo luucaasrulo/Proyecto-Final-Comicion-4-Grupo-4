0321c1dc2b3f7a01c9843f0541268a56cd755204
luucaasrulo <lucas___1997@hotmail.com>
2023-12-15 13:18:31 -0300

    Lucas

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -9,7 +9,7 @@ app_name = 'apps.usuarios'
 urlpatterns = [
     path('registrar/', RegristrarUsuario.as_view(), name='registrar'),
     path('iniciar_sesion/', LoginView.as_view(template_name = 'usuarios/iniciar_sesion.html'), name='iniciar_sesion'),
-    path('login/', LoginView.as_view(template_name = 'usuarios/iniciar_sesionmodal.html'), name='login'),
+    path('login/', LoginView.as_view(template_name = 'usuarios/iniciar_sesion_modal.html'), name='login'),
     path('cerrar_sesion/', LogoutView.as_view(), name='cerrar_sesion'),
     path("actualizar_usuario/<int:pk>", ActualizarUsuario.as_view(template_name = 'usuarios/actualizar_usuario.html'), name='actualizar_usuario'),
     path("eliminar_usuario/<int:pk>", EliminarUsuario.as_view(), name='eliminar_usuario'),
